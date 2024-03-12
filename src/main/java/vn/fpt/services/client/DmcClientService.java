f5f97f0dba080cdcf677fbe1c5e28bef9456cfb1
Khoa Vu Dang <khoavd12@fpt.com>
2024-03-12 16:08:27 +0700

    update(docs): add comment client

--- src/main/java/vn/fpt/services/client/DmcClientService.java
+++ src/main/java/vn/fpt/services/client/DmcClientService.java
@@ -5,17 +5,33 @@ import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import vn.fpt.models.auth.AuthToken;
 import vn.fpt.models.auth.DecryptAuth;
+import vn.fpt.models.auth.DmCUserInfo;
 
 @RegisterRestClient
 public interface DmcClientService {
 
+    /**
+     * API decrypt Token.
+     *
+     * @param dto the arg of object decrypt.
+     * @return AuthToken.
+     */
     @POST
     @Path("/decryptToken")
     AuthToken decryptToken(@RequestBody DecryptAuth dto);
 
+    /**
+     * API get User Permission.
+     *
+     * @param appName the arg of object.
+     * @param realmName the arg of object.
+     * @param token the arg of object.
+     *
+     * @return DmCUserInfo.
+     */
     @GET
     @Path("/getUserPermissionV2")
-    AuthToken getUserPermission(@QueryParam("appName") String appName,
-                                @QueryParam("realmName") String realmName,
-                                @QueryParam("token") String token);
+    DmCUserInfo getUserPermission(@QueryParam("appName") String appName,
+                                  @QueryParam("realmName") String realmName,
+                                  @QueryParam("token") String token);
 }
--- src/main/java/vn/fpt/services/client/IamClientService.java
+++ src/main/java/vn/fpt/services/client/IamClientService.java
@@ -12,6 +12,16 @@ import vn.fpt.models.users.UpdateUserDetails;
 @RegisterRestClient
 public interface IamClientService {
 
+    /**
+     * API create a User.
+     *
+     * @param token header for request.
+     * @param id the arg of request.
+     * @param app the arg of request.
+     * @param dto body of request.
+     *
+     * @return IamUserInfo.
+     */
     @POST
     @Path("/cads/users/{id}")
     IamUserInfo createUser(@HeaderParam(HttpHeaders.AUTHORIZATION) String token,
@@ -19,6 +29,16 @@ public interface IamClientService {
                                @QueryParam("app") String app,
                                @RequestBody UpdateUserDetails dto);
 
+    /**
+     * API update a User info.
+     *
+     * @param token header for request.
+     * @param id the arg of request.
+     * @param app the arg of request.
+     * @param dto body of request.
+     *
+     * @return IamUserInfo.
+     */
     @PUT
     @Path("/cads/users/{id}")
     IamUserInfo updateUserByID(@HeaderParam(HttpHeaders.AUTHORIZATION) String token,
@@ -26,6 +46,16 @@ public interface IamClientService {
                                @QueryParam("app") String app,
                                @RequestBody UpdateUserDetails dto);
 
+    /**
+     * API get list Users.
+     *
+     * @param token header for request.
+     * @param first query param of request.
+     * @param max query param of request.
+     * @param order query param of request.
+     *
+     * @return IamPagination<IamUserInfo>.
+     */
     @GET
     @Path("/cads/users")
     IamPagination<IamUserInfo> getUsers(@HeaderParam(HttpHeaders.AUTHORIZATION) String token,
@@ -34,6 +64,15 @@ public interface IamClientService {
                                         @QueryParam("app") String app,
                                         @QueryParam("order") String order);
 
+    /**
+     * API get a User Details.
+     *
+     * @param token header for request.
+     * @param id the arg of request.
+     * @param app the arg of request.
+     *
+     * @return AuthToken.
+     */
     @GET
     @Path("/cads/users/{id}")
     IamUserInfo getUserById(@HeaderParam(HttpHeaders.AUTHORIZATION) String token,
